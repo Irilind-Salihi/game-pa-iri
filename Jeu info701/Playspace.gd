80a1641871940975984eb2f8d642679e378db904
Morningheart <46131687+Morningheart@users.noreply.github.com>
2022-09-28 10:33:36 +0200

    Début Scène de jeu

---
+++ Jeu info701/Playspace.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+const CardSize = Vector2(125,175)
+# const CardBase = preload("res://Assets/Card/CardBase.tscn") #remplacer par le truc de IRILIND
+const PlayerHand = preload("res://Assets/Card/Player_Hand.gd")
+var CardSelected = []
+onready var DeckSize = PlayerHand.CardList.size()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _init(event):
+	if Input.is_action_jsut_released("leftclick"):
+		# Draw Card
+		var new_card = CardBase.instance()
+		CardSelected = randi() % DeckSize
+		new_card.Cardname = PlayerHand.CardList[CardSelected]
+		new_card.rect_position = get_global_mouse_position()
+		new_card.rect_scale *= CardSize/new_card.rect_size
+		$Cards.add_child(new_card)
+		PlayerHand.CardList.erase(PlayerHand.card)
+		DeckSize -= 1
+		
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
