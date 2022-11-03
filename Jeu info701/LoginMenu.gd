573430a62149941c59813dcade60cc0ece50f8bb
Irilind-Salihi <73695089+Irilind-Salihi@users.noreply.github.com>
2022-11-03 13:50:21 +0100

    [ + ] Menu D'accueil / Login form that return token

---
+++ Jeu info701/LoginMenu.gd
@@ -0,0 +1,38 @@
+extends Control
+
+var username
+var password
+var path = "res://cache.json"
+var test = {
+	"test" : "sadasdasd"
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+
+func _on_loginButton_pressed():
+	var headers = ["Content-Type: application/json"]
+	var body = {username = username,password = password}
+	body = JSON.print(body)
+	$loginContainer/loginButton/loginRequest.request("http://127.0.0.1:1234/user/login",headers, false, HTTPClient.METHOD_POST, body)
+
+
+
+func _on_usernameInput_text_changed(userText):
+	username = userText
+
+
+func _on_passwordInput_text_entered(passwordText):
+	password = passwordText
+
+func savefile (json):
+	var file = File.new()
+	file.open(path,File.WRITE)
+	file.store_string(json)
+func _on_loginRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	savefile(body)
+	print(json.result)
